fcd8bec16f87ab098cc00525a134d6da0256d3b9
K-Shinhira <144861653+K-Shinhira@users.noreply.github.com>
2024-05-17 11:55:38 +0900

    変更

--- HTML/db-connect.php
+++ HTML/db-connect.php
@@ -2,7 +2,7 @@
     const SERVER = 'mysql304.phy.lolipop.lan';
     const DBNAME = 'LAA1517449-apple';
     const USER = 'LAA1517449';
-    const PASS = 'koukou1449';
+    const PASS = '4649';
 
     $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
 ?>
