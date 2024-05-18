df141ae873bd8a7745b15bbbf9d6bb0a1cabed9d
schoolstudent16 <146916958+schoolstudent16@users.noreply.github.com>
2024-05-18 19:59:21 +0900

    Bootstrap、Vuetify勉強用フォルダ追加
    使い方を練習するときにこのフォルダにサンプルファイルを置く

---
+++ Vuetify2、Bootstrap勉強用/main.js
@@ -0,0 +1,38 @@
+const routes = [
+    { path: '/', component: httpVueLoader('./単一ファイルコンポーネント/page1.vue') },
+    { path: '/page2', component: httpVueLoader('./単一ファイルコンポーネント/page2.vue') },
+    { path: '/page3', component: httpVueLoader('./単一ファイルコンポーネント/page3.vue') },
+    { path: '/page4', component: httpVueLoader('./単一ファイルコンポーネント/page4.vue') },
+    { path: '/page5', component: httpVueLoader('./単一ファイルコンポーネント/page5.vue') },
+    { path: '/page6', component: httpVueLoader('./単一ファイルコンポーネント/page6.vue') },
+]
+
+const router = new VueRouter({
+    routes
+})
+
+new Vue({
+    el: '#app',
+    router: router,
+    vuetify: new Vuetify(),
+    data() {
+        return {
+            drawer: null,
+            supports: [
+                { name: 'Consulting and suppourt', icon: 'mdi-vuetify' },
+                { name: 'Discord community', icon: 'mdi-discord' },
+                { name: 'Report a bug', icon: 'mdi-bug' },
+                { name: 'Github issue board', icon: 'mdi-github' },
+                { name: 'Stack overview', icon: 'mdi-stack-overflow' },
+            ],
+            nav_lists: [
+                { name: 'Getting Started', route: '/' },
+                { name: 'Customization', route: '/page2' },
+                { name: 'Styles & animations', route: '/page3' },
+                { name: 'UI Components', route: '/page4' },
+                { name: 'Directives', route: '/page5' },
+                { name: 'Preminum themes', route: '/page6' },
+            ]
+        };
+    }
+});
---
+++ Vuetify2、Bootstrap勉強用/単一ファイルコンポーネント/page1.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <v-btn elevation="2">BUTTON</v-btn>
+  </div>
+</template>
+
+<script>
+module.exports = {
+  data: function() {
+    return {
+      title: 'page1.vue'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+    color: red;
+}
+</style>
