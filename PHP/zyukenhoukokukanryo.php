6e192c4e58f7a4f4c202cf67e43582c07962897a
fsho1222 <148302976+fsho1222@users.noreply.github.com>
2024-06-06 09:54:56 +0900

    zyuken

---
+++ PHP/zyukenhoukokukanryo.php
@@ -0,0 +1,85 @@
+<?php session_start(); ?>
+<!doctype html>
+<html lang="ja" data-bs-theme="auto">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>トップページ</title>
+
+    <!-- リセットCSS -->
+    <link rel="stylesheet" href="https://unpkg.com/modern-css-reset/dist/reset.min.css"/>
+    
+    <!-- bootstrap.CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- sidebar.CSS -->
+    <link href="../CSS/sidebars.css" rel="stylesheet">
+
+    <!-- DB接続 -->
+    <?php require 'db-connect.php'; ?>
+
+    <!-- common.CSS -->
+    <link href="../CSS/common.css" rel="stylesheet">
+
+    <style>
+    .background-image{
+        background-image: url("../IMAGE/school.jpg");
+        background-size: cover;
+        height: 200px;
+        width:100%;
+    }
+
+    #company-name-search-form{
+        padding: 0 3rem;
+        width: 100%;
+        height: 3rem;
+    }
+
+    #company-name-search-form button{   
+        padding: 0 1.3rem;
+    }
+    </style>
+
+</head>
+<body>
+    <!-- サイドバーとメインコンテンツのラッパー -->
+    <div class="wrapper">
+      <?php require 'sidebars.php'; ?>
+        <!-- メインコンテンツ -->
+        <main class="container-fluid main-content" style="padding: 0;">
+<!----------------------------------------------------ここから-------------------------------------------------------------------->
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <title>受験報告完了画面</title>
+</head>
+<body>
+    <h1>報告完了しました！</h1><br>
+    <h1>ご協力ありがとうございます。</h1>
+    <button type="submit" class="btn btn-primary">TOPに戻る</button>
+</body>
+</html>
+<!----------------------------------------------------ここまで-------------------------------------------------------------------->
+</main>
+    </div>
+
+    <!-- jquery.Script -->
+    <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+
+    <!-- bootstrap.Script-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- sidebar.Script-->
+    <script src="../SCRIPT/sidebars.js"></script>
+
+    <!-- common.Script-->
+    <script src="../SCRIPT/common.js"></script>
+
+    <!-- DB切断 -->
+    <?php $pdo = null;?>
+</body>
+</html>
