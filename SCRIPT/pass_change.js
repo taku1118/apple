4b4d7de3905bb7acf0e1f5bceabcc30f094d1670
TK0606 <2201137@s.asojuku.ac.jp>
2024-06-14 10:25:44 +0900

    passchange

--- SCRIPT/pass_change.js
+++ SCRIPT/pass_change.js
@@ -1,22 +1,24 @@
 $(function(){
     $("#change_confirm").click(function(){
-        if ($("#formGroupExampleInput").val() != $("#formGroupExampleInput2").val()) {
-            alert("パスワードが一致しません。もう一度入力してください。");
+        if($("#formGroupExampleInput").val() == '' || $("#formGroupExampleInput2").val() == ''){
+            alert("値が入っていません。");
         }else{
-            let password = $("#formGroupExampleInput2").val();
-            alert(password);
-            $.ajax({
-            type: 'POST',
-            url: 'change-password.php',
-            data: {pass: password}
-            }).done(function(data){
-                let result = JSON.parse(data);
-                alert(result.request[0]);
-                location.href="my_page_screen.php";
-            }).fail(function(){
-                alert("fail");
-            });
-            
+            if ($("#formGroupExampleInput").val() != $("#formGroupExampleInput2").val()) {
+                alert("パスワードが一致しません。もう一度入力してください。");
+            }else{
+                let password = $("#formGroupExampleInput2").val();
+                $.ajax({
+                type: 'POST',
+                url: 'change-password.php',
+                data: {pass: password}
+                }).done(function(data){
+                    let result = JSON.parse(data);
+                    alert(result.request[0]);
+                    location.href="my_page_screen.php";
+                }).fail(function(){
+                    alert("fail");
+                }); 
+            }
         }
     });
 })
