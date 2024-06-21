ad1a9dd246c5a92ffbcc494b30e13cc18f1b2e55
schoolstudent16 <146916958+schoolstudent16@users.noreply.github.com>
2024-06-21 10:59:16 +0900

    Create View Personal_inform修正

--- PHP/save_state.php
+++ PHP/save_state.php
@@ -1,15 +1,34 @@
 <?php
 require('db-connect.php');
-
 $datas = $_POST['ways'] ?? [];
+$insert_sql = [];
+$adopt_ids = [];
+
+
 if($_POST['inserts']!=0){
-    for($i = 0;$i<$_POST['inserts'];$i++){
-        $sql = "INSERT INTO Adopt_State_Details (adopt_id, adopt_step_id, adopt_way, adopt_date) VALUES (1,(SELECT COALESCE(MAX(adopt_step_id)+1,1) FROM adopt_state_details AS details WHERE adopt_id=?),'','')";
-        $insert_state = $pdo->prepare($sql);
-        $insert_state->execute([$_POST['adopt_id']]);
+    
+    try{
+        $pdo->beginTransaction();
+        $sql = "SELECT COALESCE(MAX(adopt_step_id)+1,1) as 'next_id' FROM adopt_state_details WHERE adopt_id=?";
+        $fetch_adopt_step_id = $pdo->prepare($sql);
+        $fetch_adopt_step_id->execute([$_POST['adopt_id']]);
+        $next_id = $fetch_adopt_step_id->fetch();
+        $adopt_step_id = $next_id["next_id"];
+        for($i = 0;$i<$_POST['inserts'];$i++){
+            $insert_sql[] = "(".$_POST['adopt_id'].",".$adopt_step_id.",'','')";
+            $adopt_step_id++;
+        }
+        $exe_sql = "INSERT INTO Adopt_State_Details VALUES ".implode(",",$insert_sql)."";
+        $pdo->exec($exe_sql);
+        $pdo->commit();
+    }catch(Exception $e){
+        // エラーが発生した場合、トランザクションを取り消す
+        $pdo->rollBack();
     }
     
 }
+
+
 $case = [];
 $ids = [];
 $exe_value = [];
@@ -19,6 +38,7 @@ for($i = 0; $i<count($datas); $i++){
     $exe_value[] = $i+1;
     $exe_value[] = $_POST['ways'][$i];
 }
+
 
 // CASE文の中身を生成
 $increment_case = implode(" ", $case);
@@ -37,8 +57,9 @@ try {
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $update_select_state = $pdo->prepare($sql);
     $update_select_state->execute($exe_value);
-    echo json_encode(['status' => 'success', 'data' => "test"]);
+    echo json_encode(['status' => 'success', 'data' => $_POST['adopt_id']]);
 } catch (PDOException $e) {
     echo json_encode(['status' => 'error', 'message' => $e->getMessage()]);
 }
+
 exit;
--- SCRIPT/select_conditon.js
+++ SCRIPT/select_conditon.js
@@ -1,20 +1,31 @@
+// インサートした回数
 let insert_count = 0;
-
+let delete_input_num = [];
 // シートの編集ボタンを押したときの動作
 function edit_sheet(target){
     let edit_div = document.getElementById("edit_area"+target.id.substring(8));
-    let icon_btn = document.getElementById("add_input"+target.id.substring(8));
+    let add_btn = document.getElementById("add_input"+target.id.substring(8));
+    let note_id = document.getElementById("note_"+target.id.substring(8));
     edit_div.className = "card-body";
     target.className = "btn btn-primary d-none";
     let save_btn = document.getElementById("save_btn"+target.id.substring(8));
     save_btn.className = "btn btn-primary";
-    icon_btn.className = "btn";
+    add_btn.className = "btn";
+    note_id.className = "mb-3";
 }
 
 // シートをクリックして開いた時の動作
 function open_sheet(target){
     let modal_num = document.querySelector("#modal_num"+target.id.substring(12)+" #modal_dialog #modal_content #card #edit_area"+target.id.substring(12));
     modal_num.className = "card-body pe-none";
+    let note_id = document.getElementById("note_"+target.id.substring(12));
+    note_id.className = "mb-3 pe-none";
+    let add_btn = document.getElementById("add_input"+target.id.substring(12));
+    add_btn.className = "btn d-none";
+    let edit_btn = document.querySelector("button[id^=edit_btn"+target.id.substring(12)+"]");
+    edit_btn.className = "btn btn-primary";
+    let save_btn = document.querySelector("button[id^=save_btn"+target.id.substring(12)+"]");
+    save_btn.className = "btn btn-primary d-none";
 }
 
 // シートの保存ボタンを押したときの動作
@@ -23,29 +34,35 @@ function save_sheet(target){
     const formData = new FormData();
     const ways = document.querySelectorAll("input[id^=step_"+target.id.substring(8)+"]");
     let update_values = Array.from(ways).map((way) => way.value);
+    let update_ids = Array.from(ways).map((way) => way.id);
     update_values.forEach((element)=>{
         formData.append("ways[]",element);
+    });
+
+    update_ids.forEach((element)=>{
+        formData.append("ids[]",element.substring(7));
     });
     
     // トップの選考IDをDB側に伝えるためにFormDataに追加
     formData.append("adopt_id",target.id.substring(8));
     formData.append("inserts",insert_count);
     // DB通信開始
-    fetch("save_state.php",{
-        method: "POST",
-        body: formData
-    }).then(response => response.json())
-    .then(data => {
-        console.log("Success:", data["data"]);
-    }).catch(error => {
-        console.log("Error:", error);
-    });
+    // fetch("save_state.php",{
+    //     method: "POST",
+    //     body: formData
+    // }).then(response => response.json())
+    // .then(data => {
+    //     console.log("Success:", data["data"]);
+    // }).catch(error => {
+    //     console.log("Error:", error);
+    // });
     target.className = "btn btn-primary d-none";
     let edit_btn = document.getElementById("edit_btn"+target.id.substring(8));
-    let icon_btn = document.getElementById("add_input"+target.id.substring(8));
+    let add_btn = document.getElementById("add_input"+target.id.substring(8));
     edit_btn.className = "btn btn-primary";
     edit_div.className = "card-body pe-none";
-    icon_btn.className = "btn d-none";
+    add_btn.className = "btn d-none";
+    // console.log(insert_count,update_values.length);
     // 保存し終えたのインサート回数のリセット
     insert_count = 0;
 }
@@ -70,14 +87,63 @@ function add_input(target){
     let input_num = target.id.substring(9);
     let card_body = document.getElementById("edit_area"+input_num+"");
     let input_part = document.querySelectorAll("input[id^=step_"+input_num+"]");
+    
     let count_input = input_part.length+1;
-    // インサートされたことをDBに伝えるための準備
     insert_count++;
-    const input_html = `<div class="text-center">
-                            <i class="bi bi-caret-down-fill" style="font-size: 3rem;"></i>
-                        </div>
-                        <div class="card-text" id="input_${count_input}">
-                            <input type="text" class="form-control form-control-lg" id="step_${input_num}_${count_input}" value="">
+    const input_html = `<div id="adopt_area">
+                            <div class="text-center">
+                                <i class="bi bi-caret-down-fill" style="font-size: 3rem;"></i>
+                            </div>
+                            <div class="card-text position-relative" id="input_${count_input}">
+                                <input type="text" class="form-control form-control-lg" id="step_${input_num}_${count_input}" value="">
+                                <button class="btn btn-danger position-absolute top-0 start-100 translate-middle btn-sm rounded-5">✕</button>
+                            </div>
                         </div>`;
     card_body.insertAdjacentHTML("beforeend",input_html);
+}
+
+
+// 削除の仕様
+// 今まで作成したUPDATEやINSERTだと実装できない可能性がある
+// その理由として連番が問題になっている
+// 変数「insert_count」にインサートした回数、「count_input」にシート内のinputの総数を管理している
+// count_inputはinputに連番を振るためにある
+// しかしcount_inputは追加ボタンを押したときのinputの総数からとってきている
+// もしもともと連番を振っていたinputが4つ「1，2，3、4」と並んでいた場合...
+// 2番目を削除する
+// すると連番は「1，3，4」となる
+// こうなるとcount_inputの値は3であるが次に振られるべき番号は5となる
+// ここで矛盾が発生する
+
+// これを解決するには...
+// とその前に
+// このcount_inputがどんな役割をしているのか考えてみよう
+// count_inputはinputに連番を振るため
+// なぜ連番を振る必要があるの？
+// そもそもID属性には一意の連番を振ることが必要であるという記事を見たことがあるから振っているだけでその連番自体を何かに使う（例えば：直接データベースの値になるとか）わけではないのでは？
+// おそらく、一意のIDが振られたinputをメインに使うところがデータベースにFETCH APIで接続するタイミング
+// 話を戻すと
+// そもそも別にIDを一意に振る必要はないのでは？
+// データベースに格納するのは「選考ID（シートに持たせる）、選考ステップID（DBの状態から自動で生成）、選考内容（inputのvalue）、日付（自動生成）」4つ
+
+// 次はDB側の話
+// DB側ではAUTO_INCREMENTは使用していない
+// 理由は単純な連番ではなく、選考IDごとの連番だから
+// 今の連番の振り方は選考IDごとの選考ステップIDの総数を数えて、それに1プラスすることで連番を振っている
+// これだと削除した場合、連番の振り直しが必須となる
+// なぜならば例えば選考ステップID「1，2，3，4」があったとする
+// そのうちの2番目が削除されると「1，3，4」となる
+// 次にインサートされるときは「3」となり、重複エラーが出てしまう
+// これはDBなのでHTMLのように無視はできない
+
+// 解決するには....
+// 連番を振り直すのがいいのか？
+// この場合、削除したらいちいち連番を振り直す処理を行わなければいけないがそっちの方が確実なためそっちを採用する
+
+
+function delete_input(target){
+    let target_id = target.id.substring(14);
+    let adopt_area = document.getElementById("adopt_area"+target_id+"");
+    delete_input_num.push(target_id);
+    adopt_area.remove();
 }
