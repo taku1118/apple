03ba6e0ab16ec9af9907a0797480fb6b63e4915b
K-Shinhira <144861653+K-Shinhira@users.noreply.github.com>
2024-06-26 13:32:56 +0900

    a

--- PHP/change-password.php
+++ PHP/change-password.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 require ('db-connect.php');
-    $pass=password_hash([$_POST['pass']], PASSWORD_DEFAULT);
+    $pass=password_hash($_POST['pass'], PASSWORD_DEFAULT);
     $sql=$pdo->prepare('update Users set password=? where student_number = ?');
     $sql->execute([$pass,$_SESSION['user']['student_number']]);
     echo json_encode(["request" => [$_POST['pass']]]);
