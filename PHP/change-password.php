cf340d593910e1e8613e42f29be642de2087da40
TK0606 <2201137@s.asojuku.ac.jp>
2024-06-26 12:23:45 +0900

    passchange

--- PHP/change-password.php
+++ PHP/change-password.php
@@ -1,8 +1,10 @@
 <?php
 session_start();
 require ('db-connect.php');
-    $user_id = "0000000";
-    $test_query = $pdo->prepare("UPDATE users SET password = ? WHERE student_number = '0000000'");
-    $test_query->execute([$_POST['pass']]);
+    $pass=password_hash([$_POST['pass']], PASSWORD_DEFAULT);
+    $sql=$pdo->prepare('update Users set password=? where student_number = ?');
+    $sql->execute([$pass,$_SESSION['user']['student_number']]);
     echo json_encode(["request" => [$_POST['pass']]]);
 exit;
+// $test_query = $pdo->prepare("UPDATE users SET password = ? WHERE student_number = '0000000'");
+//     $test_query->execute([$_POST['pass']]);
