407ee321495cd59f4cda63d77940718b9e3193a2
TK0606 <2201137@s.asojuku.ac.jp>
2024-06-27 10:40:49 +0900

    unko

--- PHP/pass_change.php
+++ PHP/pass_change.php
@@ -22,6 +22,7 @@
     <!-- common.CSS -->
     <link href="../CSS/common.css" rel="stylesheet">
 
+    <!-- bootstrap icon -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css"/>
 
     <style>
