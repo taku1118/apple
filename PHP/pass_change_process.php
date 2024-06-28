b695ba5be3a90263b2607b2bfde4db1a295336bc
TK0606 <2201137@s.asojuku.ac.jp>
2024-06-28 10:36:21 +0900

    strlen

--- PHP/pass_change_process.php
+++ PHP/pass_change_process.php
@@ -1,12 +1,14 @@
 <?php
 session_start();
 require ('db-connect.php');
-if (preg_match("/^[a-zA-Z0-9]+$/", $_POST['pass'])) {
+if(strlen($_POST['pass']) > 4) {
+    echo "文字数がオーバーしています。";
+}else if (preg_match("/^[a-zA-Z0-9]+$/", $_POST['pass'])) {
     $pass=password_hash($_POST['pass'], PASSWORD_DEFAULT);
     $sql=$pdo->prepare('update Users set password=? where student_number = ?');
     $sql->execute([$pass,$_SESSION['user']['student_number']]);
     echo json_encode(["request" => [$_POST['pass']]]);
-} else {
+}else {
     echo "すべて半角英数ではない";
 }
 exit;
