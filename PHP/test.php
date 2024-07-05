7cad4f3960b2484fc78911da2121942edad0c8f0
K-Shinhira <144861653+K-Shinhira@users.noreply.github.com>
2024-07-05 10:09:39 +0900

    henkou

--- PHP/test.php
+++ PHP/test.php
@@ -1,6 +1,6 @@
 <?php
 require 'db-connect.php';
-$pass=password_hash('0000', PASSWORD_DEFAULT);
+$pass=password_hash('test', PASSWORD_DEFAULT);
 $sql=$pdo->prepare('update Users set password=? where student_number = ?');
 $sql->execute([$pass,'0000000']);
 ?>
